a5155717232370da595f5015cbe85b80e8835dd9
Cristopher Yates <cmoyates@gmail.com>
2022-05-06 02:36:41 -0230

    Set up a basic socket.io server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
 import { Application, Request, Response } from "express";
+import { Server } from "socket.io";
+
 
 const app: Application = express();
-const PORT: number = 5000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Response");
+app.get("/", (_: Request, res: Response) => {
+  res.send("The server is running!");
 })
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+const PORT: number = 5000;
+
+let server = app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+
+
+const io = new Server(server,  {
+  cors: {
+    origin: ["http://localhost:8080"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  }
+});
+
+io.on("connection", socket => {
+  console.log(socket.id);
+
+  socket.on("test-event", () => {
+    console.log("test");
+  })
+});
